ad6789f25b046d0f2ec80c5f0ab0f76482211c42
Johan Córdoba Peraza <49567115+johancordoba78@users.noreply.github.com>
2026-02-10 10:54:50 -0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,41 +1,21 @@
-// ===============================
-// MAPAS BASE
-// ===============================
-
 const baseMaps = {
   "OpenStreetMap": L.tileLayer(
     "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
     { attribution: "© OpenStreetMap" }
-  ),
-
-  "Carto Claro": L.tileLayer(
-    "https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png",
-    { attribution: "© CARTO" }
   )
 };
-
-// ===============================
-// DATOS CREF
-// ===============================
 
 const CREF_DATA = {
   "BORUCA": {
     clasif: "CREF y PAFs",
-    area: {
-      2023: 1485.9,
-      2024: 1509.26
-    },
+    area: { 2023: 1485.9, 2024: 1509.26 },
     adenda: "SI",
     rosa: "PENDIENTE",
     pendiente: "NO"
   },
-
   "CABECAR TALAMANCA": {
     clasif: "CREF y PAFs",
-    area: {
-      2023: 25408,
-      2024: 25409.16
-    },
+    area: { 2023: 25408, 2024: 25409.16 },
     adenda: "SI",
     rosa: "PENDIENTE",
     pendiente: "NO"
